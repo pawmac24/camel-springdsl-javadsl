0e0db59db12bcff4816f0735ee1c4719205116bc
Pawel Mackiewicz PGS <pmackiewicz@pgs-soft.com>
2016-08-19 14:51:51 +0200

    Initial commit: camel-spring and Router in Java

---
+++ src/main/java/com/pawel/fuse/example/ContentBasedFileRouter.java
@@ -0,0 +1,24 @@
+package com.pawel.fuse.example;
+
+import org.apache.camel.spring.SpringRouteBuilder;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by pmackiewicz on 2016-08-19.
+ */
+@Component
+public class ContentBasedFileRouter extends SpringRouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        from("file:src/data?noop=true")
+            .choice()
+                .when(xpath("/person/city = 'London'"))
+                    .log("UK messesage")
+                    .to("file:target/messages/uk")
+                .otherwise()
+                    .log("Other message")
+                    .to("file:target/messages/others");
+    }
+}
