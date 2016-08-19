a06a5f30f5e7ffa0899b402725f875f6a121d2de
Pawel Mackiewicz PGS <pmackiewicz@pgs-soft.com>
2016-08-19 17:01:29 +0200

    Getting data from header and body inside exchange

--- src/main/java/com/pawel/fuse/example/MyProcessor.java
+++ src/main/java/com/pawel/fuse/example/MyProcessor.java
@@ -10,8 +10,17 @@ public class MyProcessor implements Processor {
 
     @Override
     public void process(Exchange exchange) throws Exception {
-        System.out.println("Some processing: "
-                + exchange.getIn().getHeader("CamelFileName"));
+
+        System.out.println("Some processing MessageId : " + exchange.getIn().getMessageId());
+
+        System.out.println("Some processing CamelFileName : " + exchange.getIn().getHeader("CamelFileName"));
+        System.out.println("Some processing CamelFileLength : " + exchange.getIn().getHeader("CamelFileLength"));
+        System.out.println("Some processing CamelFileAbsolutePath : " +  exchange.getIn().getHeader("CamelFileAbsolutePath"));
+        System.out.println("Some processing CamelFilePath : " + exchange.getIn().getHeader("CamelFilePath"));
+        System.out.println("Some processing CamelFileContentType : " + exchange.getIn().getHeader("CamelFileContentType"));
+        System.out.println("Some processing CamelFileLastModified : " + exchange.getIn().getHeader("CamelFileLastModified"));
+
+        System.out.println("Message In in Body " + exchange.getIn().getBody());
 
     }
 }
