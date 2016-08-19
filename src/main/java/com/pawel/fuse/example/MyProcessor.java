cec24fb914155b94edf6c96c59cc60ccb4662f7a
Pawel Mackiewicz PGS <pmackiewicz@pgs-soft.com>
2016-08-19 17:14:10 +0200

    Using constants

--- src/main/java/com/pawel/fuse/example/MyProcessor.java
+++ src/main/java/com/pawel/fuse/example/MyProcessor.java
@@ -13,12 +13,12 @@ public class MyProcessor implements Processor {
 
         System.out.println("Some processing MessageId : " + exchange.getIn().getMessageId());
 
-        System.out.println("Some processing CamelFileName : " + exchange.getIn().getHeader("CamelFileName"));
-        System.out.println("Some processing CamelFileLength : " + exchange.getIn().getHeader("CamelFileLength"));
+        System.out.println("Some processing CamelFileName : " + exchange.getIn().getHeader(Exchange.FILE_NAME) );
+        System.out.println("Some processing CamelFileLength : " + exchange.getIn().getHeader(Exchange.FILE_LENGTH));
         System.out.println("Some processing CamelFileAbsolutePath : " +  exchange.getIn().getHeader("CamelFileAbsolutePath"));
-        System.out.println("Some processing CamelFilePath : " + exchange.getIn().getHeader("CamelFilePath"));
-        System.out.println("Some processing CamelFileContentType : " + exchange.getIn().getHeader("CamelFileContentType"));
-        System.out.println("Some processing CamelFileLastModified : " + exchange.getIn().getHeader("CamelFileLastModified"));
+        System.out.println("Some processing CamelFilePath : " + exchange.getIn().getHeader(Exchange.FILE_PATH));
+        System.out.println("Some processing CamelFileContentType : " + exchange.getIn().getHeader(Exchange.FILE_CONTENT_TYPE));
+        System.out.println("Some processing CamelFileLastModified : " + exchange.getIn().getHeader(Exchange.FILE_LAST_MODIFIED));
 
         System.out.println("Message In in Body " + exchange.getIn().getBody());
 
