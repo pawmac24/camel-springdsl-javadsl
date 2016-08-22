00ee0fa6562321c58528c0918cc1c66bd05e038b
Pawel Mackiewicz PGS <pmackiewicz@pgs-soft.com>
2016-08-22 14:44:28 +0200

    Adding some logs to routes

--- src/main/java/com/pawel/fuse/example/CxfSoapRouter.java
+++ src/main/java/com/pawel/fuse/example/CxfSoapRouter.java
@@ -12,12 +12,16 @@ public class CxfSoapRouter extends SpringRouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        from("cxf:bean:orderEndpoint")
+        from("cxf:bean:orderEndpoint").routeId("orderEndpointRouteId")
             .process(new MyProcessor())
             .to("seda:incomingOrders")
-            .transform(constant("OK"));
+            .transform(constant("OK"))
+            .log("Operation name from headers = '${header.operationName}' after transform")
+            .log("Body = '${body}' after transform");
 
-        from("seda:incomingOrders")
+        from("seda:incomingOrders").routeId("incomingOrdersRouteId")
+            .log("Operation name from headers = '${header.operationName}' inside incomingOrders")
+            .log("Body = '${body}' inside incomingOrders")
             .to("mock:end");
     }
 }
