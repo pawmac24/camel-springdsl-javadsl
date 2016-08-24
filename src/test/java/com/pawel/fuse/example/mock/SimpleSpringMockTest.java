901f96fea299e87936f8704692c116fbe7e34400
Pawel Mackiewicz PGS <pmackiewicz@pgs-soft.com>
2016-08-24 16:55:33 +0200

    Adding mock tests

--- src/test/java/com/pawel/fuse/example/mock/SimpleSpringMockTest.java
+++ src/test/java/com/pawel/fuse/example/mock/SimpleSpringMockTest.java
@@ -1,10 +1,13 @@
 package com.pawel.fuse.example.mock;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.spring.CamelSpringTestSupport;
 import org.junit.Test;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
 
 public class SimpleSpringMockTest extends CamelSpringTestSupport {
 
@@ -17,14 +20,22 @@ public class SimpleSpringMockTest extends CamelSpringTestSupport {
     public void testMockFoo() throws Exception {
         MockEndpoint resultEndpoint = getMockEndpoint("mock:foo");
 
-        resultEndpoint.expectedMessageCount(2);
-        resultEndpoint.expectedBodiesReceived("Hello World", "Hate World");
+        resultEndpoint.expectedMessageCount(3);
+        resultEndpoint.expectedBodiesReceived("Hello World", "Hate World", "Funny World");
 
         template.sendBody("direct:start", "Hello World");
         template.sendBody("direct:start", "Hate World");
+        template.sendBody("direct:start", "Funny World");
 
         // now lets assert that the mock:foo endpoint received 2 messages
         resultEndpoint.assertIsSatisfied();
+
+        List<Exchange> exchangeList = resultEndpoint.getExchanges();
+        assertEquals(3, exchangeList.size());
+
+        assertTrue(exchangeList.get(0).getIn().getBody(String.class).contains("World"));
+        assertTrue(exchangeList.get(1).getIn().getBody(String.class).contains("World"));
+        assertTrue(exchangeList.get(2).getIn().getBody(String.class).contains("World"));
     }
 
     @Test
@@ -39,6 +50,12 @@ public class SimpleSpringMockTest extends CamelSpringTestSupport {
 
         // now lets assert that the mock:foo endpoint received 2 messages
         resultEndpoint.assertIsSatisfied();
+
+        List<Exchange> exchangeList = resultEndpoint.getExchanges();
+        assertEquals(2, exchangeList.size());
+
+        assertTrue(exchangeList.get(0).getIn().getBody(String.class).contains("Hello"));
+        assertTrue(exchangeList.get(1).getIn().getBody(String.class).contains("Hate"));
     }
 
     @Test
@@ -53,6 +70,12 @@ public class SimpleSpringMockTest extends CamelSpringTestSupport {
 
         // now lets assert that the mock:foo2 endpoint received 2 messages
         resultEndpoint.assertIsSatisfied();
+
+        List<Exchange> exchangeList = resultEndpoint.getExchanges();
+        assertEquals(2, exchangeList.size());
+
+        assertTrue(exchangeList.get(0).getIn().getBody(String.class).contains("Bye"));
+        assertTrue(exchangeList.get(1).getIn().getBody(String.class).contains("Bye"));
     }
 
 }
