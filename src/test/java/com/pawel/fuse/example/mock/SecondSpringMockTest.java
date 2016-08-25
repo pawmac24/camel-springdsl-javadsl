3ad3c0428a9ed75ba5048cc4ceb0ee8bbd4231c6
Pawel Mackiewicz PGS <pmackiewicz@pgs-soft.com>
2016-08-25 12:33:20 +0200

    Adding some tests with mocks

--- src/test/java/com/pawel/fuse/example/mock/SecondSpringMockTest.java
+++ src/test/java/com/pawel/fuse/example/mock/SecondSpringMockTest.java
@@ -1,11 +1,14 @@
 package com.pawel.fuse.example.mock;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.spring.CamelSpringTestSupport;
 import org.junit.Test;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
 
 /**
  * Created by pmackiewicz on 2016-08-25.
@@ -79,4 +82,70 @@ public class SecondSpringMockTest extends CamelSpringTestSupport {
         assertNotNull(context.hasEndpoint("mock:direct:first"));
         assertNotNull(context.hasEndpoint("mock:direct:second"));
     }
+
+    @Test
+    public void testWhatInsideInExchangeInDirectFirst() throws Exception {
+        context.getRouteDefinitions().get(0).adviceWith(context, new AdviceWithRouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                // mock all endpoints
+                mockEndpoints();
+            }
+        });
+
+        MockEndpoint mockDirectFirst = getMockEndpoint("mock:direct:first");
+        mockDirectFirst.expectedBodiesReceived("Hello World", "Hate World", "Funny World");
+        mockDirectFirst.expectedMessageCount(3);
+
+        template.sendBodyAndHeader("direct:start", "Hello World", "myCommand", "firstCommand");
+        template.sendBodyAndHeader("direct:start", "Hate World", "myCommand", "firstCommand");
+        template.sendBodyAndHeader("direct:start", "Funny World", "myCommand", "firstCommand");
+
+        //or use this for all mock endpoints
+        assertMockEndpointsSatisfied();
+
+        List<Exchange> exchangeList = mockDirectFirst.getExchanges();
+        assertEquals(3, exchangeList.size());
+        assertTrue(exchangeList.get(0).getIn().getBody(String.class).contains("Hello World"));
+        assertTrue(exchangeList.get(1).getIn().getBody(String.class).contains("Hate World"));
+        assertTrue(exchangeList.get(2).getIn().getBody(String.class).contains("Funny World"));
+    }
+
+    @Test
+    public void testWhatInsideInExchangeInDirectFirstSecondVersion() throws Exception {
+        context.getRouteDefinitions().get(0).adviceWith(context, new AdviceWithRouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                // mock only direct:first
+                mockEndpoints("direct:first");
+            }
+        });
+
+        MockEndpoint mockDirectFirst = getMockEndpoint("mock:direct:first");
+        mockDirectFirst.expectedBodiesReceived("Hello World", "Hate World", "Funny World");
+        mockDirectFirst.message(0).body().contains("World");
+        mockDirectFirst.message(0).body().startsWith("Hello");
+
+        mockDirectFirst.message(1).body().contains("World");
+        mockDirectFirst.message(1).body().startsWith("Hate");
+
+        mockDirectFirst.message(2).body().contains("World");
+        mockDirectFirst.message(2).body().startsWith("Funny");
+
+        //mockDirectFirst.allMessages().body().contains("World");
+        mockDirectFirst.expectedMessageCount(3);
+
+        template.sendBodyAndHeader("direct:start", "Hello World", "myCommand", "firstCommand");
+        template.sendBodyAndHeader("direct:start", "Hate World", "myCommand", "firstCommand");
+        template.sendBodyAndHeader("direct:start", "Funny World", "myCommand", "firstCommand");
+
+        //or use this for all mock endpoints
+        assertMockEndpointsSatisfied();
+        //
+        // check which endpoints was mocked or not mocked
+        assertNull(context.hasEndpoint("mock:direct:start"));
+        assertNotNull(context.hasEndpoint("mock:direct:first"));
+        assertNull(context.hasEndpoint("mock:direct:second"));
+    }
 }
+
